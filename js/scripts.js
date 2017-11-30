b13bb59aee6dad8c36177dc9336d6b2b55c21f46
Damian Juzyk <damian.juzyk@gmail.com>
2017-12-01 00:18:57 +0400

    add zadanie 9.2

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,21 @@ var triangle1Area = getTriangleArea(10, 15);
 var triangle2Area = getTriangleArea(1,7);
 var triangle3Area = getTriangleArea(1,90);
 var triangle4Area = getTriangleArea(10,-1);
+
+//zadanie 9.2
+
+var femaleNames = ["Asia", "Kasia", "Ola", "Jola"];
+var maleNames = ["Piotrek", "Marek", "Arek", "Jarek"];
+
+var allNames = femaleNames.concat(maleNames);
+
+console.log(allNames);
+
+var newName = "Marian";
+
+if (allNames.indexOf(newName) == -1) {
+  allNames.push(newName);
+  console.log(allNames);
+} else {
+  console.log("There's Marian already!");
+}
