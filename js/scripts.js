5fe318082e3068aff3db7bef6689ef8991771108
Damian Juzyk <damian.juzyk@gmail.com>
2017-12-01 15:36:24 +0400

    add zadanie 9.3

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,18 @@ if (allNames.indexOf(newName) == -1) {
 } else {
   console.log("There's Marian already!");
 }
+
+// zadanie 9.3
+
+var text = "Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.";
+var dinosaur = "triceratops";
+var bigDinosaur = dinosaur.toUpperCase();
+console.log(bigDinosaur);
+
+var textAfter = text.replace("Velociraptor", bigDinosaur);
+console.log(textAfter);
+
+var finalText = textAfter.substr(0, textAfter.length/2);
+console.log(finalText);
+
+// 
