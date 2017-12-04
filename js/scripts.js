cc5baf76fba2fc9a574342074a23e6e19b3fb02a
Damian Juzyk <damian.juzyk@gmail.com>
2017-12-04 11:14:45 +0600

    add poprawione 9.6

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,10 @@
 
 add.addEventListener("click", function(){
   var element = document.createElement("li");
-  element.innerHTML = "item";
+  var number = document.getElementsByTagName("li").length;
+  element.innerHTML = "item " + number;
   list.appendChild(element);
           }
     );
+    // pytanie
+    //kod działa (dodając kolejne elementy item 1, item 2, ...) po wyłaczeniu wcześniejszych elementów kodu w HTML (dotyczacych pozycji "li"); w jaki sposob mozna bezposrednio odniesc sie do elementu  aby nie kolidiowalo to ze wczesniejszymi elemtami list?
